42dd956f341808d7526530d29ead3d54ad92e8de
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-15 12:44:24 -0600

    added explanation comment

--- IntroToPhp/HowIsPHPExcecuted.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "I Love PHP!";?>
-
-<!-- note the semi-colon and double quotes -->
---
+++ IntroToPhp/HowIsPHPExecuted.php
@@ -0,0 +1,6 @@
+<?php
+echo "I Love PHP!";?>
+
+<!-- note the semi-colon and double quotes -->
+
+<!-- can also be executed in the terminal eg: php IntroToPhp/howIsPhpExecuted -->
