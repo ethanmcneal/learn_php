729a26fad500633f926ed547481649105a63ed87
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-15 12:52:01 -0600

    new lines

--- IntroToPhp/HowIsPHPExecuted.php
+++ IntroToPhp/HowIsPHPExecuted.php
@@ -4,3 +4,16 @@ echo "I Love PHP!";?>
 <!-- note the semi-colon and double quotes -->
 
 <!-- can also be executed in the terminal eg: php IntroToPhp/howIsPhpExecuted -->
+
+
+<!-- Unlike many other languages, PHP is not always case-sensitive, 
+so Echo is a valid statement in PHP. However, it’s best practice to use standard casing – in this case, 
+echo. -->
+
+
+<?php
+echo"1. Learn PHP"; 
+echo "\n2. Get a job";
+echo "\n3. Get a \"wife\""?>
+
+<!-- \n for newline, \"\" for printing literal quotes -->
